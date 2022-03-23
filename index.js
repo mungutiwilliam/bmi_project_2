3e6d3ba9d35aa77c3605dba8beef2daf1aaab05b
William <mungutiwilliam97@gmail.com>
2022-03-23 18:48:19 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const bodyParser = require('body-parser');
+const urlEncodedParser = bodyParser.urlencoded({extended: false});
+
+app.set('views','views');
+app.set('view engine', 'hbs');
+app.use(express.static('public'));
+
+
+app.get('/contacts', function(request, response){
+    response.render('contact_us');
+});
+app.get('/bmi-calculator', function(request, response){
+    response.render('bmi');
+})
+
+
+app.post('/process-contacts', urlEncodedParser, function(request, response){
+    response.end('Thankyou ' +request.body.first_name + ' ' +request.body.last_name);
+});
+
+app.post('/bmi-results', urlEncodedParser, function(request, response){
+    response.end('Your height is '+request.body.height+ ' and weight is '+request.body.weight);
+})
+
+app.listen(port);
+console.log('Server is listening to port 3000');
