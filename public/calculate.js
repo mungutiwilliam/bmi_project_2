0f95c04ef9f9aaa34f73dd7902dccd022e644a7a
William <mungutiwilliam97@gmail.com>
2022-03-31 15:17:48 +0300

    Detailed output

--- public/calculate.js
+++ public/calculate.js
@@ -4,20 +4,20 @@ function calculate(height,weight){
     const h_squared = h*h 
     const bmi_ans =  w/h_squared 
     if (bmi_ans < 18.5){
-        return alert('Underweight')  
+        return alert(`Your height is ${height}m and weight is ${weight}kgs bmi =${bmi_ans}, Summary = Underweight`)  
     } 
     else if(bmi_ans < 24.9){
         
         
-        return alert('Healthy Weight')    
+        return alert(`Your height is ${height}m and weight is ${weight}kgs bmi =${bmi_ans}, Summary = Healthy weight`)    
     }
     else if(bmi_ans < 29.9){
         
-        return alert('Overweight')
+        return alert(`Your height is ${height}m and weight is ${weight}kgs bmi =${bmi_ans}, Summary = Overweight`)
         
     }else {
         
-        return alert('Obese')   
+        return alert(`Your height is ${height}m and weight is ${weight}kgs bmi =${bmi_ans}, Summary = Obese`)   
     }     
 }
 module.exports = {calculate};
